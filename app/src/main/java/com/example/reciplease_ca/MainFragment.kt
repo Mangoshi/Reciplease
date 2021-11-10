14193521a25414ff40b3e76aba3e6f5536580710
mangoshi <soggymoose@gmail.com>
2021-11-10 23:42:21 +0000

    Added a RecyclerView to the main fragment

--- app/src/main/java/com/example/reciplease_ca/MainFragment.kt
+++ app/src/main/java/com/example/reciplease_ca/MainFragment.kt
@@ -6,16 +6,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.reciplease_ca.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = MainFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        // Now we have references to all the child view objects within the layout
+        // Now we have a code block where we can reference the object multiple times
+        with(binding.recyclerView) {
+            // Height of each row will be the same regardless of contents
+            setHasFixedSize(true)
+            val divider = DividerItemDecoration(
+                context, LinearLayoutManager(context).orientation
+            )
+            // Creating a visual divider between each of the rows
+            addItemDecoration(divider)
+        }
+
+
+        return binding.root
+
     }
 }
