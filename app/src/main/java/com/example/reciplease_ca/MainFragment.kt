08922f3977eaba3b546800d05d40da2205c455c8
mangoshi <soggymoose@gmail.com>
2021-11-15 23:55:48 +0000

    Can now pass selected data to a fragment

--- app/src/main/java/com/example/reciplease_ca/MainFragment.kt
+++ app/src/main/java/com/example/reciplease_ca/MainFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.reciplease_ca.databinding.MainFragmentBinding
@@ -51,5 +52,7 @@ class MainFragment : Fragment(),
 
     override fun onItemClick(noteId: Int) {
         Log.i(TAG, "onItemClick: received note id $noteId")
+        val action = MainFragmentDirections.actionEditNote(noteId)
+        findNavController().navigate(action)
     }
 }
