9e5d8f60292dd5250fc81394aa5c6c0f10a205ff
mangoshi <soggymoose@gmail.com>
2021-11-16 00:10:47 +0000

    Can now navigate back to a previous destination

--- app/src/main/java/com/example/reciplease_ca/EditorFragment.kt
+++ app/src/main/java/com/example/reciplease_ca/EditorFragment.kt
@@ -4,8 +4,12 @@ import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.reciplease_ca.databinding.EditorFragmentBinding
 
@@ -18,10 +22,44 @@ class EditorFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_check)
+        }
+        setHasOptionsMenu(true)
+
         binding = EditorFragmentBinding.inflate(inflater, container, false)
         binding.editor.setText("You selected note number ${args.noteid}")
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    saveAndReturn()
+                }
+            }
+        )
+
         return binding.root
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> saveAndReturn()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveAndReturn(): Boolean {
+        // Go back to the main fragment
+        findNavController().navigateUp()
+        return true
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
