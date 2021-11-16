c4e843ec8872bff843502bee9af3d1eca5c5ed94
mangoshi <soggymoose@gmail.com>
2021-11-16 14:41:39 +0000

    Turned data class into a room entity

--- app/src/main/java/com/example/reciplease_ca/data/NoteEntity.kt
+++ app/src/main/java/com/example/reciplease_ca/data/NoteEntity.kt
@@ -1,14 +1,18 @@
 // This is a primary building block in saving data to memory and persistent storage
 package com.example.reciplease_ca.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.reciplease_ca.NEW_NOTE_ID
 import java.util.*
 
-// "data" means there will be some properties used
-data class NoteEntity(
-    var id: Int,
-    var date: Date,
-    var text: String
+// Defining the structure of the app's database table
+// "data" in "data class" means there will be some properties used by this class
+@Entity(tableName = "notes")
+data class NoteEntity(@PrimaryKey(autoGenerate = true)
+                      var id: Int,
+                      var date: Date,
+                      var text: String
 ) {
     constructor() : this(NEW_NOTE_ID, Date(), "")
     constructor(date: Date, text: String) : this(NEW_NOTE_ID, date, text)
